54752d745320d5df58f808ea2a2221d2b854a6d4
gbrielll <105085023+GbrielZanoni@users.noreply.github.com>
2023-09-11 21:37:24 -0300

    arruma o pytest

--- test_site/test_app.py
+++ test_site/test_app.py
@@ -1,14 +1,28 @@
+import unittest
+import os
+import sys
+
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+# Importamos nosso app
 from app import app
+
+# Importamos a biblioteca de testes
 import unittest
 
-class TestApp(unittest.TestCase):
+
+class TestHomeView(unittest.TestCase):
 
     def setUp(self):
-        self.app = app.test_client()
+        ap = app.test_client()
+        self.response = ap.get('/')
+        self.response_P1 = ap.get('/secondpage')
 
-    def test_index(self):
-        response = self.app.get('/')
-        self.assertEqual(response.status_code, 200)
+    # Testamos se a resposta e 200 ("ok")
+    def test_get(self):
+        self.assertEqual(200, self.response.status_code)
+        self.assertEqual(200, self.response_P1.status_code)
 
-if __name__ == '__main__':
-    unittest.main()
+    # Testamos se o content_type da resposta da home esta correto
+    def test_content_type(self):
+        self.assertIn('text/html', self.response.content_type)
+        self.assertIn('text/html', self.response_P1.content_type)
