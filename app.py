693db005dbbb61a0d2ad336873b6b19a04020d04
gbrielll <105085023+GbrielZanoni@users.noreply.github.com>
2023-09-11 21:33:08 -0300

    ajustes e nova página

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
-from flask import Flask
+from flask import Flask, render_template
 
-app = Flask(__name__)
 
+app = Flask(__name__) 
 @app.route('/')
-def index():
-    return "Hello, World!"
+def Home():
+    return render_template("Homepage.html")
 
-if __name__ == "__main__":
-    app.run()
+@app.route('/secondpage')
+def Projeto1():
+    return render_template("secondpage.html")
+
