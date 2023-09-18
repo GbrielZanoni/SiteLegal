86f587d9dbc42b2df90956873bd420286e419227
gb <105085023+GbrielZanoni@users.noreply.github.com>
2023-09-18 20:00:23 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
 from flask import Flask, render_template
 
-app = Flask(__name__) 
+
+app = Flask(__name__)
+
 @app.route('/')
 def Home():
     return render_template("Homepage.html")
 
-@app.route('/secondpage')
+@app.route('/projeto1')
 def Projeto1():
-    return render_template("secondpage.html")
-
+    return render_template("projeto1.html")
